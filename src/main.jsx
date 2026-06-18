8bb46d5402f6bbfbd50f56fe5547629d51a74132
Adriel23 <adrieldev23@gmail.com>
2026-06-17 23:24:04 -0300

    DespliegueFront

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import ThemeContextProvider from './Context/ThemeContext.jsx'
+import AuthContextProvider from './Context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <ThemeContextProvider>
-            <App />
+            <AuthContextProvider>
+                <App />
+            </AuthContextProvider>
         </ThemeContextProvider>
     </BrowserRouter>
 )
